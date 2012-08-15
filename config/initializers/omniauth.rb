e2d22d3dc2309c7ef52e8b1d56363eab6876c529
dodgerogers@hotmail.com <andy.rogers@teebox.co>
2012-08-15 20:47:46 +0100

    canvas true

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '123858854427503', '2dac7ebd85f62f31403322ac4331600d',
-  scope: "publish_stream, email, user_photos", iframe: true , authorize_params: { display: "popup" }
+  scope: "publish_stream, email, user_photos", canvas: true 
 end
